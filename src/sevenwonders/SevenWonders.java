2ab271c6aca9a9b34751559699a977ffa0b702f8
rfloweree <rubenloweree@gmail.com>
2020-01-25 11:10:26 -0700

    Initial commit.

---
+++ src/sevenwonders/SevenWonders.java
@@ -0,0 +1,16 @@
+package sevenwonders;
+
+/**
+ * 
+ * @author ruben
+ */
+public class SevenWonders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
